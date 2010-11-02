32badbff4a5e0aea93a54b17947ab2a19fdc4d3a
Brandon Turner <bturner@bltweb.net>
2010-11-02 18:23:11 -0500

    Refactored master/slave replication

--- manifests/classes/master.pp
+++ manifests/classes/master.pp
@@ -2,11 +2,19 @@ class mysql::master inherits mysql::server {
 
   Augeas["my.cnf/replication"] {
     changes => [
-      "set log-bin mysql-bin",
+      "set log-bin mysqld-bin",
       "set server-id ${mysql_serverid}",
       "set expire_logs_days 7",
       "set max_binlog_size 100M"
     ],
   }
 
+  if $mysql_binlog_format {
+    augeas { "my.cnf/binlog-format":
+      context => "$mycnfctx/mysqld/",
+      changes => [
+        "set binlog-format ${mysql_binlog_format}"
+      ],
+    }
+  }
 }
