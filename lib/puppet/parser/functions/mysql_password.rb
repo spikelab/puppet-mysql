7acfbe17dc5f085e7e2711c37aee0e76b169b742
Spike Morelli <fsm@spikelab.org>
2011-02-22 16:36:45 +0000

    fix problem with empty passwords in the mysql_password function

--- lib/puppet/parser/functions/mysql_password.rb
+++ lib/puppet/parser/functions/mysql_password.rb
@@ -1,9 +1,14 @@
 # hash a string as mysql's "PASSWORD()" function would do it
+# mysql treats an empty password differently and does not generate an hash for it
 require 'digest/sha1'
 
 module Puppet::Parser::Functions
 	newfunction(:mysql_password, :type => :rvalue) do |args|
-		'*' + Digest::SHA1.hexdigest(Digest::SHA1.digest(args[0])).upcase
+                hash = ''
+                if args[0] != ''
+                    hash = '*' + Digest::SHA1.hexdigest(Digest::SHA1.digest(args[0])).upcase
+                end
+                hash
 	end
 end
 
